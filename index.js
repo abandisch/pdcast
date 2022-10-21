0ccf68ed000b0ef497557abc9d74a3264c74e53a
Alex Bandisch <alexander.bandisch@gmail.com>
2022-10-22 07:53:03 +1000

    trying to setup player

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 import Root from './src/containers/Root';
 import {name as appName} from './app.json';
+import {PlaybackService} from './src/containers/TrackPlayer/services';
 
 AppRegistry.registerComponent(appName, () => Root);
+TrackPlayer.registerPlaybackService(() => PlaybackService);
--- src/containers/Episode.tsx
+++ src/containers/Episode.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../lib/types';
+import {SetupService} from './TrackPlayer/services';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Episode'>;
 
@@ -9,9 +10,22 @@ function Episode({navigation, route}: Props) {
   console.log('navigation:', navigation);
   console.log('route:', route);
 
+  const [isPlayerReady, setIsPlayerReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function run() {
+      const isSetup = await SetupService();
+      setIsPlayerReady(isSetup);
+    }
+
+    run();
+  }, []);
+
   const handleGoBack = () => {
     navigation.pop();
   };
+
+  console.log('isPlayerReady', isPlayerReady);
 
   return (
     <View>
